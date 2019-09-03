3200e29225f8d3ae2163e06188eaa4084814b942
Ketchp <54551120+Ketchp@users.noreply.github.com>
2019-09-03 21:28:44 +0200

    Skoro hotovo

--- test/test.js
+++ test/test.js
@@ -1,5 +1,5 @@
 const Stats = require('../lib/stats');
-
+/*
 const INTERVALS = [ 1000, 5 * 1000, 15 * 1000, 60 * 1000, 5 * 60 * 1000 ];
 
 function simulate( interval, samples, min, max )
@@ -32,3 +32,36 @@ function simulate( interval, samples, min, max )
 }
 
 simulate( 30 * 1000, 10, 10, 100 );
+*/
+
+function simulate(samples, min, max, val_bucket_size, time_bucket_size, history_time, tick, interval)
+{
+    let start = Date.now()
+
+    const stats = new Stats(history_time, time_bucket_size, val_bucket_size);
+
+    result = {};
+
+    for(let i = 0; i < samples; i++)
+    {
+        stats.push(min + Math.random() * ( max - min ), start + i * tick - Math.random()*45);
+        console.log(stats.buckets/*[stats.pointer].sub*/);
+    
+        if(i % 7 == 0)
+        {    
+            result =
+            {
+                cnt : stats.cnt( interval ),
+                min : stats.min( interval ),
+                max : stats.max( interval ),
+                avg : stats.avg( interval ),
+                mdn : stats.mdn( interval ),
+                sum : stats.sum( interval )
+            };
+            console.log(result);
+        }
+    }
+
+}
+
+simulate(45, 150, 600, 25, 100, 400, 30, 200);
