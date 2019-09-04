8009604dbb6000e8650ac5aeca2238c58f260c97
Stefan Slavkovsky <slavkovsky.stefan01@gmail.com>
2019-09-04 21:41:31 +0200

    Prerobene
    Prerobil som to ako si chcel
    Este tam neni median a tiez value_interval(velkost bucketu na values) je zatial fixna
    tiez som do constructoru pridal precision

--- test/test.js
+++ test/test.js
@@ -1,16 +1,17 @@
 const Stats = require('../lib/stats');
-/*
+
 const INTERVALS = [ 1000, 5 * 1000, 15 * 1000, 60 * 1000, 5 * 60 * 1000 ];
 
 function simulate( interval, samples, min, max )
 {
     let end = Date.now(), start = end - interval, tick = ( end - start ) / samples;
 
-    const stats = new Stats( INTERVALS );
+    const stats = new Stats( 10, ...INTERVALS );
 
     for( let i = 0; i < samples; ++i )
     {
         stats.push( min + Math.random() * ( max - min ), start + i * tick );
+        //console.log(stats.intervals[0]);
     }
 
     let result = {};
@@ -19,21 +20,21 @@ function simulate( interval, samples, min, max )
     {
         result[interval] =
         {
-            cnt : stats.min( interval ),
+            cnt : stats.cnt( interval ),
             min : stats.min( interval ),
-            max : stats.min( interval ),
-            avg : stats.min( interval ),
-            mdn : stats.min( interval ),
-            sum : stats.min( interval )
+            max : stats.max( interval ),
+            avg : stats.avg( interval ),
+            mdn : stats.mdn( interval ),
+            sum : stats.sum( interval )
         }
     }
 
     console.log( result );
 }
 
-simulate( 30 * 1000, 10, 10, 100 );
-*/
+simulate( 1000, 10, 10, 100 );
 
+/*
 function simulate(samples, min, max, val_bucket_size, time_bucket_size, history_time, tick, interval)
 {
     let start = Date.now()
@@ -45,7 +46,6 @@ function simulate(samples, min, max, val_bucket_size, time_bucket_size, history_
     for(let i = 0; i < samples; i++)
     {
         stats.push(min + Math.random() * ( max - min ), start + i * tick - Math.random()*45);
-        console.log(stats.buckets/*[stats.pointer].sub*/);
     
         if(i % 7 == 0)
         {    
@@ -65,3 +65,4 @@ function simulate(samples, min, max, val_bucket_size, time_bucket_size, history_
 }
 
 simulate(45, 150, 600, 25, 100, 400, 30, 200);
+*/
